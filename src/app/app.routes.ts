c0626ffcf8445582536c1d4b36c8f7c26a0a7c7d
Rosamelanillo67 <marc.fradera@institutvidreres.cat>
2025-05-27 16:18:05 +0200

    ChatIA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { importProvidersFrom } from '@angular/core';
 import { LoginComponent } from './components/login/login.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
+import { IaBotComponent } from './components/ia-bot/ia-bot.component';
 
 export const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'ia-bot', component: IaBotComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }
 ];
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)]
+  providers: [provideRouter(appRoutes),provideHttpClient()]
 }).catch(err => console.error(err));
