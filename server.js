22eec311ea404af748e843393d107b59adebad09
Rosamelanillo67 <marc.fradera@institutvidreres.cat>
2025-05-28 13:57:40 +0200

    ComitFinal

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const mysql = require('mysql2');
+const axios = require('axios');
+const cors = require('cors');
+const path = require("path");
+
+const app = express();
+app.use(cors());
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'localBotiga'
+});
+
+connection.connect((err) =>{
+  if (err) throw err;
+  console.log('Estas connectat a la BD')
+})
+
+app.use(express.json());
+app.post('/api/compras', (req, res) => {
+  const { nombre, cantidad, moneda } = req.body;
+
+  const sql = 'INSERT INTO compras (nombre_producto, cantidad_comprada, moneda) VALUES (?, ?, ?)';
+  connection.query(sql, [nombre, cantidad, moneda], (err, result) => {
+    if (err) {
+      console.error('Error al insertar en la BD:', err);
+      return res.status(500).send('Error al guardar la compra');
+    }
+    res.status(200).send('Compra guardada correctamente');
+  });
+});
+
+
+app.get('/api/exchange-rates', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,binancecoin&vs_currencies=usd,eur'
+    );
+
+    const data = response.data;
+    console.log('Datos recibidos de CoinGecko:', data);
+
+    const btcUsd = data.bitcoin.usd;
+    const btcEur = data.bitcoin.eur;
+    const bnbUsd = data.binancecoin.usd;
+
+
+    const rates = {
+      usd: 1,
+      eur: btcEur / btcUsd,
+      bnb: bnbUsd,
+      btc: btcUsd,
+    };
+
+    res.json(rates);
+  } catch (error) {
+    console.error('Error al obtener las tasas:', error.message);
+    res.status(500).json({ message: 'Error fetching exchange rates' });
+  }
+});
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
-import { NgForOf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { NgForOf, CommonModule  } from '@angular/common';
 import { CartService } from '../../services/cart.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ethers } from 'ethers';
+
+
+type Currency = 'usd' | 'eur' | 'bnb' | 'btc';
+
+declare let window: any;
+const BTCB_CONTRACT_ADDRESS = '0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8';
+const ERC20_ABI = [
+  "function transfer(address to, uint256 amount) external returns (bool)",
+  "function balanceOf(address account) external view returns (uint256)"
+];
+
+
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   standalone: true,
-  imports: [NgForOf]
+  imports: [NgForOf, CommonModule, HttpClientModule, FormsModule]
 })
-export class CartComponent {
-  constructor(public cartService: CartService) {}
+export class CartComponent implements OnInit{
+  constructor(public cartService: CartService, private http: HttpClient) {}
+
+  selectedCurrency: Currency = 'usd';
+
+
+  exchangeRates: { [key in Currency]: number } = {
+    usd: 1,
+    eur: 1,
+    bnb: 1,
+    btc: 1
+  };
+  ngOnInit() {
+    this.http.get<{ [key in Currency]: number }>('http://localhost:3000/api/exchange-rates')
+      .subscribe(rates => {
+        this.exchangeRates = rates;
+      });
+  }
+
+  convertPrice(price: number): number {
+    return price / this.exchangeRates[this.selectedCurrency];
+  }
+
 
   get groupedItems() {
     return this.cartService.getGroupedCart();
@@ -19,10 +55,140 @@ export class CartComponent {
     return this.cartService.getTotal();
   }
 
-  pay() {
-    alert('Gracias por tu compra');
+  payUSD() {
+
+    for (const item of this.groupedItems) {
+      const payload = {
+        nombre: item.name,
+        cantidad: item.quantity,
+        moneda: 'USD'
+      };
+
+      this.http.post('http://localhost:3000/api/compras', payload).subscribe();
+    }
+
+    alert('Gracias por tu compra')
     this.cartService.clearCart();
   }
+
+  payEUR() {
+
+    for (const item of this.groupedItems) {
+      const payload = {
+        nombre: item.name,
+        cantidad: item.quantity,
+        moneda: 'EUR'
+      };
+
+      this.http.post('http://localhost:3000/api/compras', payload).subscribe();
+    }
+
+    this.cartService.clearCart();
+    alert('Gracias por tu compra')
+  }
+
+  async payWithBNB() {
+    if (!window.ethereum) {
+      alert('MetaMask no está instalado');
+      return;
+    }
+
+    try {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const sender = accounts[0];
+
+      // Linea per evitar Spam Filter
+      const amountBNB = this.convertPrice(this.total);
+      if (amountBNB < 0.001) {
+        alert('El monto es muy bajo para enviar con BNB, intenta agregar más productos.');
+        return;
+      }
+
+      const value = this.toHexWei(amountBNB.toFixed(6));
+
+      const txParams = {
+        from: sender,
+        to: '0xbF794c99990079dAECbd70F647Cc2BB54e540B04',
+        value,
+      };
+
+      const txHash = await window.ethereum.request({
+        method: 'eth_sendTransaction',
+        params: [txParams],
+      });
+
+      console.log('TX enviada:', txHash);
+      alert('Pago con BNB enviado, Gracias por tu compra');
+
+      //BD
+      for (const item of this.groupedItems) {
+        const payload = {
+          nombre: item.name,
+          cantidad: item.quantity,
+          moneda: 'BNB'
+        };
+
+        this.http.post('http://localhost:3000/api/compras', payload).subscribe();
+      }
+      this.cartService.clearCart();
+
+    } catch (err: any) {
+      console.error(err);
+      alert('Error al enviar BNB: ' + err.message);
+    }
+  }
+
+  toHexWei(amountBNB: string): string {
+    const wei = BigInt(parseFloat(amountBNB) * 1e18);
+    return '0x' + wei.toString(16);
+  }
+
+  async payWithBTCB() {
+
+    if (!window.ethereum) {
+      alert('MetaMask no está instalado');
+      return;
+    }
+
+    try {
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const signer = await provider.getSigner();
+
+      const btcb = new ethers.Contract(BTCB_CONTRACT_ADDRESS, ERC20_ABI, signer);
+
+      const totalBTC = this.convertPrice(this.total).toFixed(8);
+
+      const amount = ethers.parseUnits(totalBTC, 18);
+      const recipient = await signer.getAddress();
+
+      const tx = await (btcb as any).transfer(recipient, amount);
+      await tx.wait();
+
+      alert('Pago con BTCB en testnet completado, Gracias por tu compra');
+
+      //BD
+      for (const item of this.groupedItems) {
+        const payload = {
+          nombre: item.name,
+          cantidad: item.quantity,
+          moneda: 'BTC'
+        };
+
+        this.http.post('http://localhost:3000/api/compras', payload).subscribe();
+      }
+
+      this.cartService.clearCart();
+    } catch (err: any) {
+      console.error(err);
+      alert('Error al enviar BTCB: ' + err.message);
+    }
+  }
+
+
+
 }
 
 
